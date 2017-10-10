581f68d4833681d64b65b73bb55f813b79526819
Dominic Chapman <dominicchapman@me.com>
2017-10-10 22:17:10 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -8,4 +8,10 @@ class Author
     @posts = []
   end
 
+  # Author #name #add_post takes in an argument of a post and adds that post to the author's collection and tells the post that it belongs to that author
+  def add_post(post)
+    @posts << post
+    post.author = self
+  end
+
 end
