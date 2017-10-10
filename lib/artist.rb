18d565140f4f95cffe003a1d7161e0a4585632f1
Dominic Chapman <dominicchapman@me.com>
2017-10-10 21:36:50 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,4 +9,10 @@ class Artist
     @songs = []
   end
 
+  # Artist #name #add_song takes in an argument of a song and adds that song to the artist's collection and tells the song that it belongs to that artist
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
 end
