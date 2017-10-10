d08d906128922f9e45f52b90542a60d33b8b1e0a
Dominic Chapman <dominicchapman@me.com>
2017-10-10 22:20:54 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -14,12 +14,17 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+    @@post_count += 1
   end
 
   # Author #name #add_post_by_title takes in an argument of a post title, creates a new post with it and associates the post and author
   def add_post_by_title(post_title)
     post_title = Post.new
     self.add_post(post_title)
+  end
+
+  def post_count
+    @@post_count
   end
 
 end
